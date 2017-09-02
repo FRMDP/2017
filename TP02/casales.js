ba4cf238b3480eb3e22b19e36372c0ad6f54d207
ignaciocasales <casales.ignacio@gmail.com>
2017-09-02 17:34:14 -0300

    Code Refactor

--- TP02/casales.js
+++ TP02/casales.js
@@ -3,53 +3,42 @@
  *
  * @param Array reposApiResponse.
  */
-function userStats(reposApiResponse) {
-    const repos = reposApiResponse;
-
-    /*
-     * Returns the number of repos.
-     */
-    this.getReposCount = () => {
-        return repos.length;
-    };
-
-    /*
-     * Returns the sum of stars of all repos.
-     */
-    this.getStarsCount = () => {
-        let stars = 0;
-        repos.forEach((elem) => {
-            stars += elem.stargazers_count;
-        })
-        return stars;
-    };
-
-    /*
-     * Returns the average of stars of all repos.
-     */
-    this.getStarsAvg = () => {
-        return this.getStarsCount() / this.getReposCount();
-    };
-
-    /*
-     * Returns the sum of watchers of all repos.
-     */
-    this.getWatchersCount = () => {
-        let watchers = 0;
-        repos.forEach((elem) => {
-            watchers += elem.watchers_count;
-        })
-        return watchers;
-    };
-
-    /*
-     * Returns the sum of Forks of all repos.
-     */
-    this.getForksCount = () => {
-        let forks = 0;
-        repos.forEach((elem) => {
-            forks += elem.forks;
-        })
-        return forks;
-    };
+function UserStats(reposApiResponse) {
+    this.repos = reposApiResponse;
 }
+
+/*
+ * Returns the number of repos.
+ */
+UserStats.prototype.getReposCount = function () {
+    return this.repos.length
+};
+
+/*
+ * Returns the sum of stars of all repos.
+ */
+UserStats.prototype.getStarsCount = function () {
+    return this.repos.reduce((acc, curr) => acc + curr.stargazers_count, 0)
+};
+
+
+/*
+ * Returns the average of stars of all repos.
+ */
+UserStats.prototype.getStarsAvg = function () {
+    return (this.getStarsCount() / this.getReposCount()).toFixed(2)
+};
+
+/*
+ * Returns the sum of watchers of all repos.
+ */
+UserStats.prototype.getWatchersCount = function () {
+    return this.repos.reduce((acc, curr) => acc + curr.watchers_count, 0)
+};
+
+/*
+ * Returns the sum of Forks of all repos.
+ */
+UserStats.prototype.getForksCount = function () {
+    return this.repos.reduce((acc, curr) => acc + curr.forks, 0)
+};

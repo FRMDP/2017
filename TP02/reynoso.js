f8e8c2c9e6e793113c58196ac6dc2f7b6c847926
Reynoso Matías <soloamimepasa@hotmail.com>
2017-09-02 18:21:02 -0300

    cambiados nombres de variables/funciones, uso de Array extra, forEach y reduce tambien (por las dudas)

--- TP02/reynoso.js
+++ TP02/reynoso.js
@@ -1,12 +1,11 @@
 
-function estrellas_no_fork(jsonresponse){
-		  let result = 0;
-			for (let i of jsonresponse){
-				
-        if (i.fork==false) {
-          result+= i.stargazers_count;        }
-      }
-      console.log("tenes "+result+" estrellas");
-			return result;
-		}
-   
+function CountNoForkRepoStars(userRepoList) {
+          let arrStars = [];
+          userRepoList.forEach(function (repo){
+            if (repo.fork==false) {
+              arrStars.push(repo.stargazers_count);
+            }
+          });
+        let totalStars= arrStars.reduce((total,stars)=>total+stars);
+        return totalStars;
+}

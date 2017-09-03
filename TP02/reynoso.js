d62b5e764db6ab67256058690255a1b4f2d2b0be
Reynoso Matías <soloamimepasa@hotmail.com>
2017-09-02 21:31:41 -0300

    elimine reduce() y deje forEach() mas simplificado

--- TP02/reynoso.js
+++ TP02/reynoso.js
@@ -1,11 +1,6 @@
 
 function CountNoForkRepoStars(userRepoList) {
-          let arrStars = [];
-          userRepoList.forEach(function (repo){
-            if (repo.fork==false) {
-              arrStars.push(repo.stargazers_count);
-            }
-          });
-        let totalStars= arrStars.reduce((total,stars)=>total+stars);
+        let totalStars = 0;
+        userRepoList.forEach(repo=>{if (repo.fork==false) totalStars+=(repo.stargazers_count);});
         return totalStars;
 }

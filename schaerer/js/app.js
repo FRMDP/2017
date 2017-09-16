06efddee9aeff4cf8b0b97dc3fcd8ba19d719775
DanielScha <danielschaerer96@hotmail.com>
2017-09-16 16:21:53 -0300

    tp3 listo

--- schaerer/js/app.js
+++ schaerer/js/app.js
@@ -48,8 +48,10 @@ new Vue({
 		cambiarVista(vista){
 			this.cerrarMensajes();
 			this.limpiarDatosContacto();
+			if(this.vista != "vistaEditarContacto" && vista != "vistaEditarContacto"){
+				this.filtro = '';
+			}
 			this.vista = vista;
-			this.filtro = '';
 		},
 		setContactosLocalStorage(){
 			localStorage.setItem('contactos', JSON.stringify(this.contactos));
@@ -66,19 +68,21 @@ new Vue({
 			this.mensajeContactoAgregado = true;
 			this.setContactosLocalStorage();
 		},
-		borrarContacto(indice){
-			this.contactos.splice(indice, 1);
+		borrarContacto(contact){
+			this.indiceContactoSeleccionado = this.contactos.indexOf(contact);
+			this.contactos.splice(this.indiceContactoSeleccionado, 1);
 			this.setContactosLocalStorage();
+			this.indiceContactoSeleccionado = '';
 			this.mensajeContactoEliminado = true;
 		},
-		abrirFormularioEditarContacto(indice){
+		abrirFormularioEditarContacto(contact){
+			this.indiceContactoSeleccionado = this.contactos.indexOf(contact);
 			this.cambiarVista("vistaEditarContacto");
-			this.contacto.nombre = this.contactos[indice].nombre;
-			this.contacto.apellido = this.contactos[indice].apellido;
-			this.contacto.edad = this.contactos[indice].edad;
-			this.contacto.direccion = this.contactos[indice].direccion;
-			this.contacto.sexo = this.contactos[indice].sexo;
-			this.indiceContactoSeleccionado = indice;
+			this.contacto.nombre = this.contactos[this.indiceContactoSeleccionado].nombre;
+			this.contacto.apellido = this.contactos[this.indiceContactoSeleccionado].apellido;
+			this.contacto.edad = this.contactos[this.indiceContactoSeleccionado].edad;
+			this.contacto.direccion = this.contactos[this.indiceContactoSeleccionado].direccion;
+			this.contacto.sexo = this.contactos[this.indiceContactoSeleccionado].sexo;
 		},
 		editarContacto(){
 			this.contactos.splice(this.indiceContactoSeleccionado, 1, Object.assign({}, this.contacto));

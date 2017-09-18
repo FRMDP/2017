eb963b3a0624dd5285014a7ad43e2fad08ff4e6b
Santiago Agustin Blanc <santiago.blanc@globant.com>
2017-09-18 14:34:43 -0300

    Version lista para revisar || Faltan funcionalidades adicionales

--- blanc/js/app.js
+++ blanc/js/app.js
@@ -7,24 +7,25 @@ const vm = new Vue({
             address: '',
             gender: '',
             birthdate: '',
-            numbers: [] 
+            numbers: []
         },
         contactList: [],
         filter: '',
-        view: ''
+        view: 'home'
     },
     mounted() {
         this.getContactsFromStorage();
     },
     computed: {
         searchedContacts() {
-            return this.contactList.filter(p => p.firstName.indexOf(this.filter) >= 0);
+            return this.contactList.filter(c => c.givenName.indexOf(this.filter) >= 0);
         },
         formIsCorrect() {
             return this.contact.givenName &&
                 this.contact.surName &&
                 this.contact.address &&
-                this.contact.gender;
+                this.contact.numbers &&
+                this.contact.birthdate;
         }
     },
     methods: {
@@ -47,6 +48,18 @@ const vm = new Vue({
             this.contact.address = '';
             this.contact.gender = '';
             this.contact.numbers = [];
+        },
+        deleteContact(contact){
+          let index = null;
+          for (let i = 0; i < this.contactList.length; i ++) {
+            if (this.contactList[i].numbers === contact.numbers) {
+              index = i;
+            }
+          }
+          if (index!=null) {
+            this.contactList.splice(index,1);
+            this.saveContactsToStorage(this.contactList)
+          }
         },
         changeView(view) {
             this.view = view;

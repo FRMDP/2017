7763d9d9684961b9c2666c56bdc6e2194e00ff7b
Savino <rds_213@hotmail.com>
2017-10-04 17:39:47 -0300

    Componentes 1 Rodo 0

---
+++ savino/src/components/favoritos-menu.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="favoritos-menu">
+    	<h2>Favoritos contacto</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+    	name: 'favoritosMenu'
+    }
+</script>

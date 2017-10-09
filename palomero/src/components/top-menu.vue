e332a6155dd5c3508d2d40fbc26ca08451f988d2
fedealdosivi <fedepalo13@gmail.com>
2017-10-09 19:24:24 -0300

    added vuetify removed materialize

--- palomero/src/components/top-menu.vue
+++ palomero/src/components/top-menu.vue
@@ -1,13 +1,21 @@
 <template>
-      <nav>
-       <div class="nav-wrapper">
-        <a href="#" class="brand-logo right">Mdq News</a>
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-
-        </ul>
-      </div>
-    </nav>
-
+ <v-toolbar dark class="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>search</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
 </template>
 
 <script>
--- palomero/src/main.js
+++ palomero/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './components/App.vue'
+
+Vue.use(Vuetify)
 
 new Vue({
     el: '#app',

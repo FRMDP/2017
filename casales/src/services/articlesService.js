6e881db1ab5d7a425bb900e021f80eb6b9234383
Ignacio Casales <casales.ignacio@gmail.com>
2017-10-08 23:05:16 -0300

    format date to unix

--- casales/src/services/articlesService.js
+++ casales/src/services/articlesService.js
@@ -1,6 +1,7 @@
 import storageService from "./storageService";
 import categoriesService from "./../services/categoriesService";
 import reportersService from "./../services/reportersService";
+import moment from "moment";
 
 const articles = [];
 
@@ -20,6 +21,7 @@ export default {
         article.id = this.articles.length + 1;
         article.category.name = categoriesService.getCategory(article.category.id).name;
         article.reporter.name = reportersService.getReporter(article.reporter.id).name;
+        article.date = moment(article.date).valueOf();
 
         this.articles.push(article);
 

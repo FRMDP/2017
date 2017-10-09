008c1baf5af1644b5b6593a2b72966743cbecaa5
fedealdosivi <fedepalo13@gmail.com>
2017-10-08 21:50:24 -0300

    basic structure

---
+++ palomero/src/components/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="app">
+    	<top-menu @cambiarVista="cambiarVista"></top-menu>
+    </div>
+</template>
+
+<script>
+	import topMenu from './top-menu.vue';
+
+    export default {
+    	name: 'app',
+    	components: {
+    		topMenu
+    	},
+    	data() {
+    		return {
+                Reporter: {
+                    id: number,
+                    name: string
+                },
+
+                Category: {
+                    id: number,
+                    name: string
+                },
+
+                New: {
+                    id: number,
+                    title: string,
+                    body: string,
+                    category: Category,
+                    reporter: Reporter,
+                    date: number
+                }
+            }
+        },
+
+        methods: {
+            cambiarVista(vista) {
+                this.vista=vista;
+            }
+        },
+
+        mounted()
+        {
+
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ palomero/src/components/top-menu.vue
@@ -0,0 +1,32 @@
+<template>
+      <nav>
+       <div class="nav-wrapper">
+        <a href="#" class="brand-logo right">Mdq News</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+
+        </ul>
+      </div>
+    </nav>
+
+</template>
+
+<script>
+    export default {
+    	name: 'topMenu',
+        props: ['vista'],
+    	data() {
+    		return {
+         mensaje:false
+    		}
+    	},
+    	methods: {
+    		cambiarVista(vista) {
+                this.$emit('cambiarVista', vista);
+            }
+    	}
+
+    }
+</script>
+
+<style>
+</style>

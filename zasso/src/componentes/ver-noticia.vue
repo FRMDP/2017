b405ce65d4395eb14d0003964fe7df616c50e4df
ZassoMartin <zassomartin@hotmail.com>
2017-10-31 17:53:28 -0300

    mi tp8

---
+++ zasso/src/componentes/ver-noticia.vue
@@ -0,0 +1,154 @@
+<style>
+</style>
+<template>
+
+<div class="noticia">
+  <div v-if="!New" class="box">
+      <h3>La noticia no existe</h3>
+  </div>
+  <div class="box">
+    <div class="post-header has-text-centered">
+      <img src="">
+      <h4 class="title is-8"><i>{{New.title}}</i></h4>
+    </div>
+    <hr>
+    <div class="post-content-short">
+      <p>{{New.body}}</p>
+      <div class="post-content-details">
+      </div>
+    </div>
+  </div>
+  <div class="box content">
+    <div class="post-header has-text-centered">
+      <h4 class="title is-8"><i>Comentarios</i></h4>
+    </div>
+    <article class="media" v-for="com in listaComentarios">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>{{com.usuario}}</strong>
+            <br>{{com.cuerpo}}
+            <br>
+          </p>
+        </div>
+      </div>
+    </article>
+    <article id="comment1" class="media">
+      <div id="comment2" class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Nombre</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control is-expanded has-icons-left">
+              <input class="input" type="text" placeholder="Ingrese su nombre..." v-model="comentario.usuario">
+              <span class="icon is-small is-left">
+        <i class="fa fa-user"></i>
+      </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="media-content">
+        <div class="field">
+          <p class="control">
+            <textarea class="textarea" placeholder="Agregar comentario..." v-model="comentario.cuerpo"></textarea>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control">
+            <button class="button" :disabled="!formOk" type="submit" @click.prevent="guardaComentario">Enviar Comentario</button>
+          </p>
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
+</template>
+<script>
+import storageService from '../services/storageService';
+export default {
+  name: 'verNoticia',
+  data() {
+    return {
+      New: '',
+      comentario: {
+        usuario: '',
+        cuerpo: '',
+        id: '',
+        date: '',
+        mg: 0
+      },
+      lista: [],
+      socket: ''
+    }
+  },
+  sockets: {
+    connect: function() {
+      console.log('socket connected');
+      this.socket = this.$socket;
+    },
+    notiComentario: function(val) {
+      this.cargaComentario(val);
+      storageService.storeComentario(this.lista);
+    }
+  },
+  methods: {
+    verNoticia() {
+      storageService.traerNoticia('http://localhost:8080/news/' + this.id)
+        .then((response) => {
+          this.New = response.data;
+          this.New.date = this.New.date.substr(0, 10)
+        })
+        .catch((error) => {
+
+          this.New = '';
+        })
+    },
+    verCategoria(id) {
+      this.$router.push('/category/' + id);
+    },
+    limpiarComentario() {
+      this.comentario.usuario = '';
+      this.comentario.cuerpo = '';
+      this.comentario.id = '';
+    },
+    cargaComentario(comm) {
+      this.lista.push(Object.assign({}, comm));
+    },
+    guardaComentario() {
+      this.comentario.id = this.New.uid;
+      this.comentario.date = new Date();
+      this.$socket.emit('notiComentario', this.comentario);
+      this.limpiarComentario();
+    },
+    tiempo(date) {
+      return moment(date).startOf('hour').fromNow();
+    }
+  },
+  computed: {
+    params() {
+      return this.$route.params;
+    },
+    id() {
+      return this.$route.params.id;
+    },
+    formOk() {
+      return this.comentario.usuario && this.comentario.cuerpo;
+    },
+    listaComentarios() {
+      return this.lista.filter(c => c.id == this.New.uid);
+    }
+  },
+  watch: {
+    '$route.params.id': function() {
+      this.verNoticia();
+    }
+  },
+  created() {
+    this.verNoticia();
+    this.lista = storageService.traerComentarios();
+    this.connect;
+  }
+}
+</script>

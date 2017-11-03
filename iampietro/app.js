5c2683039867a6f7bdc754bd011f790c46d7f23c
Iampietro <ratamahatta.sindominio@yahoo.com>
2017-11-03 17:44:20 -0300

    Decorator Pattern

---
+++ iampietro/app.js
@@ -0,0 +1,54 @@
+function Character() {
+ 
+  this.life = () => {
+  	return 3000;
+  };
+  this.attackDamage = () => {
+  	return 25;
+  };
+
+  this.resistance = () => {
+  	return 10;
+  }
+ 
+}
+
+// Decorator 1
+function sword( character ) {
+ 
+  const base_attack = character.attackDamage();
+  character.attackDamage = () => {
+    return base_attack + 50;
+  };
+ 
+}
+
+// Decorator 2
+function maul( character ){
+ 
+  const base_attack = character.attackDamage();
+  character.attackDamage = () => {
+    return base_attack + 75;
+  };
+ 
+}
+
+// Decorator 3
+function shield( character ){
+ 
+  const base_resistance = character.resistance();
+  character.resistance = () => {
+     return base_resistance + 20;
+  };
+ 
+}
+
+// Decorator 4
+function armor( character ){
+ 
+  const base_resistance = character.resistance();
+  character.resistance = () => {
+     return base_resistance + 50;
+  };
+ 
+}
